545d5ac352a1ac971e52528fef654fc7e5306c4d
hiddenLadder <kirill4ernega@gmail.com>
2024-07-08 14:43:36 +0800

    screen-magnifier

---
+++ src/preload.ts
@@ -0,0 +1,29 @@
+import { ipcRenderer } from "electron";
+
+ipcRenderer.on("SET_SOURCE", async (event, sourceId) => {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: false,
+      video: {
+        mandatory: {
+          chromeMediaSource: "desktop",
+          chromeMediaSourceId: sourceId,
+          minWidth: 1920,
+          maxWidth: 1920,
+          minHeight: 1080,
+          maxHeight: 1080,
+        },
+      },
+    });
+    handleStream(stream);
+  } catch (e) {}
+});
+
+async function handleStream(stream: MediaStream) {
+  const video = document.querySelector("video");
+  video.srcObject = stream;
+
+  video.onloadedmetadata = (e) => {
+    video.play();
+  };
+}
